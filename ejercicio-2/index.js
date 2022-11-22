6cc0990cd58b50f6e511497c68562e1153b29fbd
Iara Pou <iarapou21@hotmail.com>
2022-11-22 16:35:42 -0300

    Crea ejercicio 2

---
+++ ejercicio-2/index.js
@@ -0,0 +1,31 @@
+function darDatosUsuario(nombreUsuario, edadUsuario){
+    return `${nombreUsuario} tiene ${edadUsuario} años`;
+}
+
+function AvisarSiEsAdulto(edadUsuario){
+    if(edadUsuario>=18){
+        return "es un adulto";
+    } else {
+        return "no es adulto";
+    }
+}
+
+const usuarios = [{
+    nombre : "carla",
+    edad: 30
+}, 
+{   nombre : "nora",
+    edad: 21
+}, 
+{   nombre : "luis",
+    edad: 10
+}];
+
+for (let i = 0; i < usuarios.length; i++) {
+    const nombreUsuario = usuarios[i].nombre;
+    const edadUsuario = usuarios[i].edad;
+
+    console.log(
+        `${darDatosUsuario(nombreUsuario, edadUsuario)}, entonces ${AvisarSiEsAdulto(edadUsuario)}.`
+    )
+}
