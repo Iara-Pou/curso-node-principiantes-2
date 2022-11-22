69c8396c047121194e0184a2fe9c310662c34838
Iara Pou <iarapou21@hotmail.com>
2022-11-22 17:08:41 -0300

    Suma ejercicio 3

---
+++ ejercicio-3/globals.js
@@ -0,0 +1,14 @@
+////////////////////////////////objetos globales
+
+//ruta directorio
+console.log(__dirname);
+//ruta del archivo ejecutado
+console.log(__filename);
+
+//modulo js, objeto que representa el script
+console.log(module);
+//funcion que nos permite importar otros modulos
+console.log(require);
+
+//info del sistema operativo
+console.log(process);
