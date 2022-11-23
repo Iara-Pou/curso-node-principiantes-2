7272179d79b0a444663d25d3494a766baab2302c
Iara Pou <iarapou21@hotmail.com>
2022-11-23 11:05:47 -0300

    Suma ejercicio 6
    Es un ejemplo de exportar e importar funciones, con sumar, restar, multiplicar y dividir.

---
+++ ejercicio-6/modulos/operaciones.js
@@ -0,0 +1,22 @@
+function sumar(nro1, nro2) {
+    return nro1 + nro2;
+}
+
+function restar (nro1, nro2) {
+    return nro1 - nro2;
+}
+
+function multiplicar(nro1, nro2) {
+    return nro1 * nro2;
+}
+
+function dividir(nro1, nro2) {
+    return nro1 / nro2;
+}
+
+module.exports = {
+    sumar,
+    restar,
+    multiplicar,
+    dividir
+}
